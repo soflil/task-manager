f1b05e364d6d68761dcb5d99604a5f4b27d3bbd8
soflil <134602526+soflil@users.noreply.github.com>
2024-03-07 13:20:24 +0100

    add: styling

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -18,29 +18,39 @@ const TaskForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    reset,
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
     setTasks([...tasks, data.item]);
+    reset();
   };
 
   return (
     <>
       <form className="task-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="row">
-          <input
-            {...register("item", { required: true, minLength: 1 })}
-            type="search"
-            className="form-control form-input col"
-            id="item"
-            placeholder="Add item here..."
-            autoComplete="off"
-            autoFocus={true}
-          />
-          <button type="submit" className="btn btn-primary add-btn">
-            Add
-          </button>
+          <span className="col-10">
+            <input
+              {...register("item", { required: true, minLength: 1 })}
+              type="search reset"
+              className="form-control form-input"
+              id="item"
+              placeholder="Add item here..."
+              autoComplete="off"
+              autoFocus={true}
+            />
+          </span>
+          <span className="col form-span">
+            <button
+              disabled={!isValid}
+              type="submit"
+              className="btn btn-primary add-btn"
+            >
+              Add
+            </button>
+          </span>
         </div>
         {errors.item && <p className="error-message">{errors.item.message}</p>}
       </form>
